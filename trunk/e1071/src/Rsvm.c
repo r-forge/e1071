7aae7a6d08ba3212cb1c278f8a8dc79571358fbe
meyer <unknown@noreply.com>
2002-02-21 18:43:11 +0000

    *** empty log message ***
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/e1071@178 82024d28-4ff0-0310-8bd4-ba695345edfc

--- trunk/e1071/src/Rsvm.c
+++ trunk/e1071/src/Rsvm.c
@@ -14,15 +14,15 @@ struct svm_model
 				   regression/one class svm */
     int l;			/* total #SV */
     struct svm_node **SV;	/* SVs (SV[l]) */
-    double **sv_coef;	/* coefficients for SVs in decision functions
-			   (sv_coef[n-1][l]) */
+    double **sv_coef;	        /* coefficients for SVs in decision functions
+			           (sv_coef[n-1][l]) */
     double *rho;		/* constants in decision functions
 				   (rho[n*(n-1)/2]) */
     
     /* for classification only */
 
-    int *label;		/* label of each class (label[n]) */
-    int *nSV;		/* number of SVs for each class (nSV[n]) */
+    int *label;		        /* label of each class (label[n]) */
+    int *nSV;		        /* number of SVs for each class (nSV[n]) */
 				/* nSV[0] + nSV[1] + ... + nSV[n-1] = l */
     /* XXX */
     int free_sv;		/* 1 if svm_model is created by
