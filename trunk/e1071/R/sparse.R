d7ae1685ee3d5828749a6c82a00a0356985b2865
meyer <unknown@noreply.com>
2005-09-07 23:19:41 +0000

    further improvements to make codetools happier
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/e1071@366 82024d28-4ff0-0310-8bd4-ba695345edfc

--- trunk/e1071/R/sparse.R
+++ trunk/e1071/R/sparse.R
@@ -1,6 +1,5 @@
 read.matrix.csr <- function(file, fac = TRUE, ncol = NULL)
 {
-  library("methods")
   library("SparseM")
   l <- strsplit(readLines(file(file)), "[ ]+")
 
@@ -23,7 +22,7 @@ read.matrix.csr <- function(file, fac = TRUE, ncol = NULL)
 
   max.ja <- max(ja)
   dimension <- c(length(l), if (is.null(ncol)) max.ja else max(ncol, max.ja))
-  x = new("matrix.csr", ra = as.numeric(rja[,2]), ja = ja,
+  x = methods::new("matrix.csr", ra = as.numeric(rja[,2]), ja = ja,
     ia = as.integer(ia), dimension = as.integer(dimension))
   if (length(y)) 
     list(x = x, y = if (fac) as.factor(y) else as.numeric(y))
